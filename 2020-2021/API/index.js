6622c37207ca95c652d43238e176b8173b802251
cyril <cyrilknops@gmail.com>
2020-10-22 10:42:43 +0200

    added a listiner to the device topic

--- 2020-2021/API/index.js
+++ 2020-2021/API/index.js
@@ -7,6 +7,9 @@ const cors = require('cors');
 const port = 3000;
 const app = express();
 
+//public var
+var data;
+
 app.set('trust proxy', true);
 app.use(cors());
 
@@ -15,8 +18,7 @@ app.get('/', function(request, response){
 });
 
 app.get('/api', function(request, response){
-    let result = [{id:1,data:'bla bla'}];
-    response.send(JSON.stringify(result));
+    response.send(JSON.stringify(data));
 });
 
 //Broker
@@ -29,6 +31,9 @@ broker.on('clientConnected', function(client) {
 
 broker.on('published', function(packet, client) {
     console.log(packet.topic);
+    if(packet.topic == "device"){
+        data = packet.payload;
+    }
 });
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
