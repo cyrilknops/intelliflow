52d148ddbf0565f9ddf1b155a0f4883ff6c53c41
Cyril Knops <cyrilknops@gmail.com>
2020-10-23 14:16:19 +0200

    added protobuff to broker

--- 2020-2021/API/index.js
+++ 2020-2021/API/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const app = express();
 
 //public var
-var data;
+var hubJ, sensorJ, adminJ;
 
 app.set('trust proxy', true);
 app.use(cors());
@@ -17,50 +17,20 @@ app.get('/', function(request, response){
     response.sendFile(__dirname + '/html/index.html');
 });
 
-app.get('/api', function(request, response){
-    response.send(JSON.stringify(data));
+app.get('/api/hub', function(request, response){
+    response.send(JSON.stringify(hubJ));
 });
+app.get('/api/sensor', function(request, response){
+    response.send(JSON.stringify(sensorJ));
+});
+app.get('/api/admin', function(request, response){
+    response.send(JSON.stringify(adminJ));
+});
+
 
 //Broker
 var settings = {port: 1883};
 var broker = new mosca.Server(settings); //start the broker
-
-protobuf.load("awesome.proto", function(err, root) {
-    if (err)
-        throw err;
-
-    // Obtain a message type
-    var AwesomeMessage = root.lookupType("awesomepackage.AwesomeMessage");
-
-    // Exemplary payload
-    var payload = { awesomeField: "AwesomeString" };
-
-    // Verify the payload if necessary (i.e. when possibly incomplete or invalid)
-    var errMsg = AwesomeMessage.verify(payload);
-    if (errMsg)
-        throw Error(errMsg);
-
-    // Create a new message
-    var message = AwesomeMessage.create(payload); // or use .fromObject if conversion is necessary
-
-    // Encode a message to an Uint8Array (browser) or Buffer (node)
-    var buffer = AwesomeMessage.encode(message).finish();
-    // ... do something with buffer
-
-    // Decode an Uint8Array (browser) or Buffer (node) to a message
-    var message = AwesomeMessage.decode(buffer);
-    // ... do something with message
-
-    // If the application uses length-delimited buffers, there is also encodeDelimited and decodeDelimited.
-
-    // Maybe convert the message back to a plain object
-    var object = AwesomeMessage.toObject(message, {
-        longs: String,
-        enums: String,
-        bytes: String,
-        // see ConversionOptions
-    });
-});
 
 broker.on('clientConnected', function(client) {
     console.log('client connected', client.id);
@@ -68,24 +38,29 @@ broker.on('clientConnected', function(client) {
 
 broker.on('published', function(packet, client) {
     console.log(packet.topic);
-    if(packet.topic == "device"){
-        data = packet.payload;
-    }
+    let buffer = packet.payload;
+    protobuf.load("./proto/intelliflow.proto", function(err, root) {
+        if (err)
+            throw err;
+        switch(packet.topic){
+            case "sensor":
+                var sensorP = root.lookupType("intelliflow.Sensor");
+                sensorJ = sensorP.decode(buffer).toJSON();
+                console.log(sensorJ);
+                break;
+            case "hub":
+                var hubP = root.lookupType("intelliflow.Hub");
+                hubJ = hubP.decode(buffer).toJSON();
+                console.log(hubJ);
+                break;
+            case "admin":
+                var adminP = root.lookupType("intelliflow.Admin");
+                adminJ = adminP.decode(buffer).toJSON();
+                console.log(adminJ);
+                break;
+        }
+    });
 });
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
 
-protobuf.load("awesome.proto", function(err, root) {
-    var hubp = root.lookupType("intelliflow.Hub");
-    var sensorp = root.lookupType("intelliflow.Sensor");
-    var adminp = root.lookupType("intelliflow.Admin");
-    var hub = hubp.toObject(message, {
-        enums: String,  // enums as string names
-        longs: String,  // longs as strings (requires long.js)
-        bytes: String,  // bytes as base64 encoded strings
-        defaults: true, // includes default values
-        arrays: true,   // populates empty arrays (repeated fields) even if defaults=false
-        objects: true,  // populates empty objects (map fields) even if defaults=false
-        oneofs: true    // includes virtual oneof fields set to the present field's name
-    });
-});
