39892b773097eecfcb759fcb909282488db221c7
Glen-tech <11800025@student.pxl.be>
2020-10-30 14:01:07 +0100

    Testing GPIO
    - GPIO code written
    - GPIO code tested
    - 2 connection errors found

--- 2020-2021/PCB/TestCodesPCB/Output_Test_GPIOS/Output_Test_GPIOS.ino
+++ 2020-2021/PCB/TestCodesPCB/Output_Test_GPIOS/Output_Test_GPIOS.ino
@@ -1,29 +1,56 @@
-#define LED1 34
-#define LED2 35
-#define LED3 32
-#define LED4 33
+#define GPIO36 36
+#define GPIO39 39
+#define GPIO34 34
+#define GPIO35 35
+#define GPIO32 32
+#define GPIO5  5
+#define GPIO15 15
+#define GPIO2  2
+#define GPIO7  7
+#define GPIO8  8
 
 
 void setup() 
 {
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED3, OUTPUT);
+  pinMode(GPIO36, OUTPUT);
+  pinMode(GPIO39, OUTPUT);
+  pinMode(GPIO34, OUTPUT);
+  pinMode(GPIO35, OUTPUT);
+  pinMode(GPIO32, OUTPUT);
+  pinMode(GPIO5 , OUTPUT);
+  pinMode(GPIO15, OUTPUT);
+  pinMode(GPIO2,  OUTPUT);
+  pinMode(GPIO7,  OUTPUT);
+  pinMode(GPIO8,  OUTPUT);
   
 }
 void loop() 
 {
-  digitalWrite(LED1, HIGH);   
-  digitalWrite(LED2, HIGH);     
-  digitalWrite(LED3, HIGH); 
-  digitalWrite(LED4, HIGH);  
-  delay(1000); 
-  digitalWrite(LED1, LOW);   
-  digitalWrite(LED2, LOW);     
-  digitalWrite(LED3, LOW); 
-  digitalWrite(LED4, LOW);   
+  digitalWrite(GPIO36, HIGH);   
+  digitalWrite(GPIO39, HIGH);     
+  digitalWrite(GPIO34, HIGH); 
+  digitalWrite(GPIO35, HIGH);  
+  digitalWrite(GPIO32, HIGH);  
+  digitalWrite(GPIO5,  HIGH);  
+  digitalWrite(GPIO15, HIGH); 
+  digitalWrite(GPIO2,  HIGH);  
+  digitalWrite(GPIO7,  HIGH);   
+  digitalWrite(GPIO8,  HIGH); 
+
   delay(1000);
-   
-    
+
+  digitalWrite(GPIO36, LOW);   
+  digitalWrite(GPIO39, LOW);     
+  digitalWrite(GPIO34, LOW); 
+  digitalWrite(GPIO35, LOW);  
+  digitalWrite(GPIO32, LOW);  
+  digitalWrite(GPIO5,  LOW);  
+  digitalWrite(GPIO15, LOW); 
+  digitalWrite(GPIO2,  LOW);  
+  digitalWrite(GPIO7,  LOW);   
+  digitalWrite(GPIO8,  LOW); 
+
+  delay(1000);
+
+  
 }
