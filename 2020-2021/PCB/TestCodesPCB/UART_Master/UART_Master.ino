47e98efc3119546869948545df96c5fdc42bde8c
Glen-tech <11800025@student.pxl.be>
2020-11-08 23:12:38 +0100

    Update UART master
    - Worked in documents/arduino , not in github , is now updated

--- 2020-2021/PCB/TestCodesPCB/UART_Master/UART_Master.ino
+++ 2020-2021/PCB/TestCodesPCB/UART_Master/UART_Master.ino
@@ -3,18 +3,17 @@ String reading;
   
 void setup()
 {
-  Serial.begin(19200);
+  Serial.begin(115200);
 }
 void loop()
 {
-  Serial.println(sending);              // sends one byte  
+  Serial.println(sending);             
  
     while(Serial.available()) 
    {
    reading = Serial.readString();// read the incoming data as string  
-   //Serial.println(reading) string is recieved but it will send it back to the psoc4
+   Serial.println(reading);// string is recieved but it will send it back to the psoc4
    }
    
-  
   delay(1000);
 }
